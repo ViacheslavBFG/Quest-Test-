d8f2016ff777d10d122d21d6cfc3997e1aaf0e88
ViacheslavBFG <131298066+ViacheslavBFG@users.noreply.github.com>
2024-05-14 23:15:52 +0300

    refactored

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const CURRENT_YEAR = 2024;
-const CORRECT_ANSWER = "Right answer, well done";
-const ERROR_ANSWER = "Incorrect answer";
-const FIll_INPUT = "Please, fill the line ";
+"use strict";
+const modal = document.querySelector(".modal");
+const openModal = document.querySelectorAll(".show-modal");
+const overlay = document.querySelector(".overlay");
+const closeModal = document.querySelector(".close-modal");
+const closeModalFunc = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+for (let i = 0; i < openModal.length; i++) {
+  openModal[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
 
-const inputYearNode = document.querySelector(".input-year");
-const btnYearNode = document.querySelector(".btn-year");
-const outputYearNode = document.querySelector(".result-year");
+closeModal.addEventListener("click", closeModalFunc);
 
-btnYearNode.addEventListener("click", function () {
-  const answer = Number(inputYearNode.value);
-  let output = CORRECT_ANSWER;
+overlay.addEventListener("click", closeModalFunc);
 
-  if (answer !== CURRENT_YEAR) {
-    output = ERROR_ANSWER;
-  }
-  outputYearNode.innerText = output;
-  const inputValue = inputYearNode.value;
-  if (inputValue === "") {
-    outputYearNode.innerText = FIll_INPUT;
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModalFunc();
   }
 });
